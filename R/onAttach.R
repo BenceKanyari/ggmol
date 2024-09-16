d695044ee4678c569235b5e7d89e0ed8188611e4
BenceKanyari <123818102+BenceKanyari@users.noreply.github.com>
2024-09-16 18:39:21 +0200

    change font type

--- R/onAttach.R
+++ R/onAttach.R
@@ -8,8 +8,8 @@
     update_geom_defaults("line", list(size = 1.2, color = ggmol::mol_colors(1)))
     update_geom_defaults("tile", list(color = "black"))
     update_geom_defaults("col", list(fill = ggmol::mol_colors(2)))
-    update_geom_defaults("label", list(family = "Tahoma"))
-    update_geom_defaults("text", list(family = "Tahoma"))
+    update_geom_defaults("label", list(family = "David"))
+    update_geom_defaults("text", list(family = "David"))
 
     options(ggplot2.discrete.colour = ggmol::mol_colors())
     options(ggplot2.discrete.fill = ggmol::mol_colors())
