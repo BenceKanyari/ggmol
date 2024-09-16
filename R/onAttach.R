418b41c39f1c3b22ca120d1fae52a7e7e25a8419
BenceKanyari <123818102+BenceKanyari@users.noreply.github.com>
2024-09-16 20:04:46 +0200

    change fonts

--- R/onAttach.R
+++ R/onAttach.R
@@ -1,15 +1,25 @@
 .onAttach <- function(...) {
+
+
+    for (pkg in c("tidyverse", "patchwork", "grid", "ggthemes", "sysfonts")) {
+        if (!requireNamespace(pkg, quietly = TRUE)) {
+            install.packages(pkg)
+        }
+    }
+
     library(tidyverse)
     library(patchwork)
     library(grid)
     library(ggthemes)
 
+    sysfonts::font_add_google("Roboto", "Roboto")
+
     update_geom_defaults("point", list(size = 1.6, color = ggmol::mol_colors(1)))
     update_geom_defaults("line", list(size = 1.2, color = ggmol::mol_colors(1)))
     update_geom_defaults("tile", list(color = "black"))
     update_geom_defaults("col", list(fill = ggmol::mol_colors(2)))
-    update_geom_defaults("label", list(family = "David"))
-    update_geom_defaults("text", list(family = "David"))
+    update_geom_defaults("label", list(family = "Roboto"))
+    update_geom_defaults("text", list(family = "Roboto"))
 
     options(ggplot2.discrete.colour = ggmol::mol_colors())
     options(ggplot2.discrete.fill = ggmol::mol_colors())
