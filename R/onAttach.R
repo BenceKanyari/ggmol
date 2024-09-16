959aa8aa1c41dbddc1e6c1b7b56f02ad1a45d293
BenceKanyari <123818102+BenceKanyari@users.noreply.github.com>
2024-09-16 20:23:08 +0200

    remove font type

--- R/onAttach.R
+++ R/onAttach.R
@@ -12,14 +12,11 @@
     library(grid)
     library(ggthemes)
 
-    sysfonts::font_add_google("Roboto", "Roboto")
-
     update_geom_defaults("point", list(size = 1.6, color = ggmol::mol_colors(1)))
     update_geom_defaults("line", list(size = 1.2, color = ggmol::mol_colors(1)))
     update_geom_defaults("tile", list(color = "black"))
     update_geom_defaults("col", list(fill = ggmol::mol_colors(2)))
-    update_geom_defaults("label", list(family = "Roboto"))
-    update_geom_defaults("text", list(family = "Roboto"))
+
 
     options(ggplot2.discrete.colour = ggmol::mol_colors())
     options(ggplot2.discrete.fill = ggmol::mol_colors())
