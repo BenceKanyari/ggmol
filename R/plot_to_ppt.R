46c59b0bb491f83639227c8596a91c8c49a7ede0
BenceKanyari <123818102+BenceKanyari@users.noreply.github.com>
2024-10-07 12:59:33 +0200

    Update plot_to_ppt.R

--- R/plot_to_ppt.R
+++ R/plot_to_ppt.R
@@ -1,4 +1,4 @@
-function (plot = ggplot2::last_plot(), file_name = "plot.pptx",
+plot_to_ppt <- function (plot = ggplot2::last_plot(), file_name = "plot.pptx",
           width = NULL, height = NULL)
 {
     if (file_name %in% list.files()) {
