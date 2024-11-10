84c3e49e9c7586af7f715a0d2ac6fc3353b26734
BenceKanyari <123818102+BenceKanyari@users.noreply.github.com>
2024-11-10 20:16:04 +0100

    24.11.10

--- R/onAttach.R
+++ R/onAttach.R
@@ -6,7 +6,7 @@
     update_geom_defaults("point", list(size = 1.6, color = ggmol::mol_colors(1)))
     update_geom_defaults("line", list(linewidth = 1.2, color = ggmol::mol_colors(1)))
     update_geom_defaults("tile", list(color = "black"))
-    update_geom_defaults("col", list(fill = ggmol::mol_colors(1), width = .7))
+    update_geom_defaults("col", list(fill = ggmol::mol_colors(1)))
 
 
     options(ggplot2.discrete.colour = ggmol::mol_colors())
